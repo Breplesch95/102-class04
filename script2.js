9ee44912850df3319e2935bce4d535742cbad84a
Breplesch95 <123974055+Breplesch95@users.noreply.github.com>
2023-02-14 15:07:13 -0600

    rate

--- script2.js
+++ script2.js
@@ -1,1 +1,29 @@
 window.open('cougars.html','popupad','width=auto,height=auto')
+
+function userName(){
+    let thisName = prompt("What is your name?");
+    let secondName = "";
+
+    if(thisName!=secondName){
+        secondName = prompt("Please re-enter name!");
+
+    }
+    alert('Hello ${thisName}!');
+    
+}
+function tryAgain(){
+    let secondName = "";
+    let myName = prompt("What is your name?");
+    while(myName != secondName){
+        secondName = ('{myName} doesnt match our records, please try again');
+    }
+    alert ('Hello there ${secondName}');
+}
+let userAns = "";
+
+function rating(){
+    let userAns = prompt("How would you rate my webpage 1-5?");
+    for(let x = 0; x < userAns; x++){
+        document.getElementById("thisPar").innerHTML += "<img src = https://cdn.onlinewebfonts.com/svg/img_208816.png>"
+    }
+}
